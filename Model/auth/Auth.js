ef611242f152aecb73b55a5ea85075a7b5f1abab
lakshmeesh0298 <lakshmeesh0298@gmail.com>
2022-10-28 15:21:26 +0530

    add

---
+++ Model/auth/Auth.js
@@ -0,0 +1,43 @@
+const { model, Schema } = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET, JWT_EXPIRE } = require("../../config");
+
+const AuthSchema = new Schema(
+  {
+    username: {
+      type: String,
+    },
+    phonenumber: {
+      type: Number,
+    },
+    email: {
+      type: String,
+    },
+    password: {
+      type: String,
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: ["trainer", "consilar", "admin"],
+      default: "trainer",
+    },
+  },
+  { timestamps: true }
+);
+
+// AuthSchema.pre("save", async () => {
+//   let crypted = await bcrypt.genSalt(20);
+//   this.password = await bcrypt.hash(this.password, crypted);
+// });
+AuthSchema.methods.regToken = () => {
+  return jwt.sign({ id: this.id });
+};
+
+// AuthSchema.methods.matchPASSWORD = async function (enteredPassword) {
+//   console.log(enteredPassword, this.password);
+//   return await bcrypt.compare(enteredPassword, this.password);
+// };
+
+module.exports = model("auths", AuthSchema);
